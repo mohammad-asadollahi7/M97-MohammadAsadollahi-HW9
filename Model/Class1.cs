29031213e39f338819c1a040269b3917c1f432d1
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-04 11:54:39 +0430

    Add Product model class

---
+++ Model/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile Photo { get; set; }
+        public string Description { get; set; }
+    }
+}
