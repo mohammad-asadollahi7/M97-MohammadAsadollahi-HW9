298166395d8e1ea83e58729fd03ad03a9d3ae644
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-04 17:26:43 +0430

    Add edit page

--- CarStore/Pages/Products/Edit.cshtml.cs
+++ CarStore/Pages/Products/Edit.cshtml.cs
@@ -2,22 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Model;
 using Services.Abstract;
-using Services.Services;
 
 namespace CarStore.Pages.Products;
 
-public class DetailsModel : PageModel
+public class EditModel : PageModel
 {
-    private IProductRepository _productRepository;
-
-    public DetailsModel(IProductRepository productRepository)
+    private readonly IProductRepository _productRepository;
+    public EditModel(IProductRepository productRepository)
     {
         _productRepository = productRepository;
+
     }
     public Product product { get; set; }
     public void OnGet(int id)
     {
         product = _productRepository.GetProductById(id);
-        //return Page();
     }
 }
--- Model/Product.cs
+++ Model/Product.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata.Ecma335;
 
 namespace Model
@@ -6,10 +7,18 @@ namespace Model
     public class Product
     {
         public int Id { get; set; }
+
+        [Display(Name = "Name: ")]
         public string Name { get; set; }
+
         public string Barcode { get; set; }
+
+        [Display(Name = "Price: ")]
         public decimal Price { get; set; }
+
         public string PhotoPath { get; set; }
+
+        [Display(Name = "Description: ")]
         public string Description { get; set; }
     }
 }
