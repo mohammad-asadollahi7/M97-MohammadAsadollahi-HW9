4c78f988ecb27e9f9ce1320bd41aaa9bee09ea0a
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-06 10:00:37 +0430

    debugging

--- CarStore/Pages/Products/Edit.cshtml.cs
+++ CarStore/Pages/Products/Edit.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Model;
 using Services.Abstract;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarStore.Pages.Products;
 
@@ -11,7 +12,7 @@ public class EditModel : PageModel
     private readonly IProductRepository _productRepository;
     private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public EditModel(IProductRepository productRepository, 
+    public EditModel(IProductRepository productRepository,
                         IWebHostEnvironment webHostEnvironment)
     {
         _productRepository = productRepository;
@@ -26,27 +27,29 @@ public class EditModel : PageModel
         return Page();
     }
 
-
+    [Required]
     [BindProperty]
     public IFormFile? Photo { get; set; }
     public IActionResult OnPost()
     {
         if (ModelState.IsValid)
         {
+            string fileName = null;
             if (Photo != null)
             {
                 if (product.PhotoPath != null)
                 {
+                    fileName = product.PhotoPath;
                     var fullPath = Path.Combine(_webHostEnvironment.WebRootPath,
                                                   "Photo", product.PhotoPath);
                     System.IO.File.Delete(fullPath);
                 }
-                product.PhotoPath = ProcessPhoto();
+                product.PhotoPath = ProcessPhoto(fileName);
             }
             _productRepository.Update(product);
             return RedirectToPage("Index");
         }
-        
+
         return Page();
 
     }
@@ -55,7 +58,7 @@ public class EditModel : PageModel
     public bool Notify { get; set; }
 
 
-    public  string Message { get; set; }
+    public string Message { get; set; }
     public IActionResult OnPostNotification(int id)
     {
         if (Notify)
@@ -67,22 +70,19 @@ public class EditModel : PageModel
             Message = "Notification is off";
         }
         TempData["message"] = Message;
-        return RedirectToPage("Details", new { id = id});
+        return RedirectToPage("Details", new { id = id });
     }
 
-    private string? ProcessPhoto()
+    private string ProcessPhoto(string? fileName)
     {
-        string? fileName = product.PhotoPath;
-        if (Photo != null)
+        fileName = fileName ?? Photo.FileName;
+        var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "Photo");
+        var filePath = Path.Combine(folderPath, fileName);
+        using (var fs = new FileStream(filePath, FileMode.Create))
         {
-            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "Photo");
-            fileName = Path.Combine(Guid.NewGuid().ToString() + "_" + Photo.FileName);
-            var filePath = Path.Combine(folderPath, fileName);
-            using (var fs = new FileStream(filePath, FileMode.Create))
-            {
-                Photo.CopyTo(fs);
-            }
+            Photo.CopyTo(fs);
         }
+
         return fileName;
 
     }
